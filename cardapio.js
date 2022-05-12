0ec0c331bf75ae3d17efeec3909f8ee11f4b6edf
Frankllin Silva de Souza <frankllinsilva300@gmail.com>
2022-05-12 13:04:32 -0300

    first commit

---
+++ cardapio.js
@@ -0,0 +1,341 @@
+let cardapios = [
+  {
+    pizza: [
+      {
+        id: 1,
+        name: 'Mussarela',
+        description: 'Molho: Mussarela, Tomate,Azeitona e Orégano ',
+        price: [20.0, 26.0, 32.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 2,
+        name: 'Mista',
+        description: 'Molho: Mussarela, Apresuntado,Tomate e Orégano  ',
+        price: [20.0, 26.0, 32.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 3,
+        name: 'Frango C/Catupiry',
+        description: 'Molho: Mussarela, Frango ,Catupiry, Milho e Orégano  ',
+        price: [23.0, 28.0, 34.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 4,
+        name: 'Diplomata',
+        description: 'Molho: Mussarela, Frango ,Calabresa, Milho e Orégano  ',
+        price: [23.0, 28.0, 36.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 5,
+        name: 'Calabresa',
+        description: 'Molho: Mussarela, Calabresa , Cebola e Orégano  ',
+        price: [23.0, 28.0, 34.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 6,
+        name: 'Calabresa cheddar',
+        description: 'Molho: Mussarela, Calabresa , Cheddar e Orégano  ',
+        price: [24.0, 29.0, 35.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 7,
+        name: 'Bacon',
+        description: 'Molho: Mussarela, Bacon, Cebola e Orégano  ',
+        price: [23.0, 28.0, 34.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 8,
+        name: 'Portuguesa',
+        description:
+          'Molho: Mussarela, Apresuntado ,ovos, Azeitona, Tomate e Orégano',
+        price: [23.0, 28.0, 34.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 9,
+        name: '4 Queijo',
+        description: 'Molho: Mussarela, Catupiry , Cheddar, Qualho e Orégano  ',
+        price: [24.0, 29.0, 35.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 10,
+        name: 'Sertaneja',
+        description: 'Molho: Mussarela, Carne de sol, Qualho e Orégano  ',
+        price: [27.0, 36.0, 40.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 11,
+        name: 'Toscana',
+        description:
+          'Molho: Mussarela,  Carne de sol , Calabresa, Catupiry, Cebola e Orégano    ',
+        price: [28.0, 38.0, 42.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 12,
+        name: 'Á Moda da casa',
+        description:
+          'Molho: Mussarela, Apresuntado , Bacon, Frango, Cebola e Orégano    ',
+        price: [25.0, 30.0, 40.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 13,
+        name: 'Espanhola',
+        description: 'Molho: Mussarela, Apresuntado,Calabresa e Orégano  ',
+        price: [23.0, 28.0, 35.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 14,
+        name: 'Brigadeiro',
+        description:
+          'Creme de Leite, Salpicado de Mussarela, chocolate ao leite e Granulado',
+        price: [21.0, 26.0, 31.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 15,
+        name: 'Chocolate Crocante',
+        description:
+          'Creme de Leite, Salpicado  de Mussarela, Chocolate ao Leite e Amendoin ',
+        price: [23.0, 28.0, 33.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 16,
+        name: 'Cartola',
+        description:
+          'Creme de Leite, Salpicado de Mussarela, Chocolate ao Leite, Banana e Canela',
+        price: [23.0, 29.0, 34.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      },
+      {
+        id: 17,
+        name: 'Serenata',
+        description:
+          'Creme de Leite, Salpicado de Mussarela, Chocolate ao Leite e Serenata',
+        price: [23.0, 29.0, 34.0],
+        size: ['P', 'M', 'G'],
+        src: './images/pizza.png'
+      }
+    ]
+  },
+
+  {
+    Sanduiches: [
+      {
+        id: 1,
+        name: 'Bauru Esopecial',
+        description:
+          'Pão, Alface, Tomate, Ovo, Hambúrguer, Molho Rosé, Molho Cheddar, Queijo e Apresuntado',
+        price: 7.0
+      },
+      {
+        id: 2,
+        name: 'X-burguer',
+        description:
+          'Pão, Alface, Tomate, Ovo, Hambúrguer, Molho Rosé. Molho Cheddar, Queijo, Apresuntado E Salsicha',
+        price: 7.5
+      },
+      {
+        id: 3,
+        name: 'X-dog',
+        description:
+          'Pão, Alface, Tomate, Ovd, Salsicha, Molho Rosé, Milho Cheddar, Queijo E Apresuntado',
+        price: 6.5
+      },
+      {
+        id: 4,
+        name: 'X-frango',
+        description:
+          'Pão, Alface, Tomate, Ovo, Hambúrguer, Molho Rosé,Molho Cheddar, Queijo, Apresuntado E Frango Com Catupiry',
+        price: 8.0
+      },
+      {
+        id: 5,
+        name: 'X-carne',
+        description:
+          'Pão, Alface, Tomate, Ovd, Hambúrguer, Molho Rosé,Molho Cheddar, Queijo, Apresuntado E Carne De Sol',
+        price: 8.5
+      },
+      {
+        id: 6,
+        name: 'X-calabresa',
+        description:
+          'Pão, Alface, Tomate, Ovd, Hambúrguer, Molho Rosé,Molho Cheddar, Queijo, Apresuntado E Calabresa',
+        price: 8.0
+      },
+      {
+        id: 7,
+        name: 'X-bacon',
+        description:
+          'Pão, Alface, Tomate, Ovd, Hambúrguer, Molho Rosé,Molho Cheddar, Queijo, Apresuntado E Bacon',
+        price: 8.0
+      },
+      {
+        id: 8,
+        name: 'X-tudo',
+        description:
+          'Pão, Alface, Tomate, Ovo, Hambúrguer, Molho Rosé,Molho Cheddar, Queijo, Apresuntado, Frango C/ Catupiry.Calabresa E Bacon',
+        price: 10.0
+      },
+      ,
+      {
+        id: 9,
+        name: 'X-salada',
+        description: 'Pao, Alface, Tomate, E Frango',
+        price: 6.5
+      },
+      {
+        id: 10,
+        name: 'X-tudão',
+        description:
+          'Pão, Alface, Tomate, 2 Ovos, 2 Hamburgueres, Molho Rosé,Molho Cheddar, Queijo, Apresuntado, Bacon, Frango, Calabresa E Salsicha',
+        price: 12.0
+      }
+    ]
+  },
+  {
+    pateis: [
+      {
+        id: 1,
+        name: 'Queijo',
+        price: 5.0
+      },
+      {
+        id: 2,
+        name: 'Misto',
+        description: 'Queijo, Apresuntado E Oregano',
+        price: 6.0
+      },
+      {
+        id: 3,
+        name: 'Frango Com Catupiry',
+        description: 'Frango Com Catupiry',
+        price: 7.0
+      },
+      {
+        id: 4,
+        name: 'Calabresa',
+        price: 7.0
+      },
+      {
+        id: 5,
+        name: 'Carne De Sol',
+        description: 'Carne De Sol E Queijo Mussarela',
+        price: 8.0
+      },
+      {
+        id: 6,
+        name: 'Carne E Frango Com Catupiry',
+        description: 'Carne E Frango Com Catupiry',
+        price: 8.5
+      },
+      {
+        id: 7,
+        name: 'Frango Com Calabresa',
+        description: 'Frango, Calabresa E Catupiry',
+        price: 7.5
+      },
+      {
+        id: 8,
+        name: 'Carne Com Calabresa',
+        description: 'Carne, Calabresa E Catupiry',
+        price: ''
+      },
+      {
+        id: 9,
+        name: 'Carne, Calabresa E Catupiry',
+        description: 'Carne, Frango, Calabresa E Catupiry',
+        price: 9.0
+      },
+      {
+        id: 10,
+        name: '4-Queijo',
+        description: 'Carne, Frango, Calabresa E Catupiry',
+        price: 6.0
+      },
+      {
+        id: 11,
+        name: 'Sertanejo',
+        description: 'Carne De Sol, Qualho E Cebola',
+        price: ''
+      }
+    ]
+  },
+  {
+    acaiecremes: [
+      {
+        description:
+          'Leite em pó - Farinha Láctea - Ovomaltine, Coco Ralado - Granola - Amendoin - Granulado - Confete - Choco ball - Cereal - Leite Condensado - Calda de Chocolate - Calda de Morango e Banana'
+      },
+      { id: 1, size: '250ml', price: 6.0 },
+      {
+        id: 2,
+        size: '350ml',
+        rpice: 12.0
+      },
+      { id: 1, porcaoExtra: 1.0 }
+    ]
+  },
+  {
+    cachorroQuente: [
+      {
+        id: 1,
+        name: 'Cachorro Quente de Frango',
+        description: 'Pão, Salsicha, Frango, Milho e Batata Palha',
+        price: 3.5
+      },
+      {
+        id: 2,
+        name: 'Cachorro Quente de Carne ',
+        description: 'Pão,Salsicha, Frango, Carne Moída, Milho e Batata Palha',
+        price: 4.0
+      },
+      {
+        id: 3,
+        name: 'Cachorro Quente de Misto ',
+        description: 'Pão,Salsicha, Frango, Carne Moída, Milho e Batata Palha',
+        price: 4.5
+      }
+    ]
+  },
+  {
+    bebidas: [
+      { id: 1, name: 'Água Mineral 500ml', price: 2.0 },
+      { id: 2, name: 'Refrigerante 250ml', price: 2.0 },
+      { id: 3, name: 'Refrigerante Lata 350ml', price: 4.0 },
+      { id: 4, name: 'Refrigerante 1 Litro', price: 6.0 },
+      { id: 5, name: 'Suco na Água 400ml', price: 3.0 },
+      { id: 6, name: 'Suco no Leite 400ml', price: 4.0 },
+      { id: 7, name: 'Vitamina de Cupuaçu', price: 5.0 }
+    ]
+  }
+]
+window.cardapios = cardapios
---
+++ script.js
@@ -0,0 +1,128 @@
+// Function to return they same
+const get = el => document.querySelector(el)
+const getAll = el => document.querySelectorAll(el)
+
+let menu = window.cardapios
+//variable environment
+//pizza
+let pizzaaopen = menu[0].pizza
+// CachorroQuente
+let CachorroQ = menu[4].cachorroQuente
+// sanduiches
+let sanduiches2 = menu[1].Sanduiches
+// pateis
+let pateis = menu[2].pateis
+// bebidas
+let bebidas = menu[5].bebidas
+// acai and creme
+let acaiecreme = menu[3].acaiecremes
+
+// functions
+function pizzaopen() {
+  let flexopen = (get('.cardapio-pizza').style.display = 'flex')
+  get('.cardapio-Quente').style.display = 'none'
+  get('.cardapio-sanduíches').style.display = 'none'
+  get('.cardapio-açaiecreme').style.display = 'none'
+  get('.cardapio-drink').style.display = 'none'
+  get('.cardapio-pateis').style.display = 'none'
+}
+function cachoQuente() {
+  get('.cardapio-Quente').style.display = 'flex'
+  get('.cardapio-pizza').style.display = 'none'
+  get('.cardapio-sanduíches').style.display = 'none'
+  get('.cardapio-açaiecreme').style.display = 'none'
+  get('.cardapio-drink').style.display = 'none'
+  get('.cardapio-pateis').style.display = 'none'
+}
+function sanduiches() {
+  get('.cardapio-sanduíches').style.display = 'flex'
+  get('.cardapio-pizza').style.display = 'none'
+  get('.cardapio-Quente').style.display = 'none'
+  get('.cardapio-açaiecreme').style.display = 'none'
+  get('.cardapio-drink').style.display = 'none'
+  get('.cardapio-pateis').style.display = 'none'
+}
+function pasteis() {
+  get('.cardapio-pateis').style.display = 'flex'
+  get('.cardapio-pizza').style.display = 'none'
+  get('.cardapio-Quente').style.display = 'none'
+  get('.cardapio-sanduíches').style.display = 'none'
+  get('.cardapio-açaiecreme').style.display = 'none'
+  get('.cardapio-drink').style.display = 'none'
+}
+function bebida() {
+  get('.cardapio-drink').style.display = 'flex'
+  get('.cardapio-pizza').style.display = 'none'
+  get('.cardapio-Quente').style.display = 'none'
+  get('.cardapio-sanduíches').style.display = 'none'
+  get('.cardapio-açaiecreme').style.display = 'none'
+
+  get('.cardapio-pateis').style.display = 'none'
+}
+function cremeacai() {
+  get('.cardapio-açaiecreme').style.display = 'flex'
+  get('.cardapio-pizza').style.display = 'none'
+  get('.cardapio-Quente').style.display = 'none'
+  get('.cardapio-sanduíches').style.display = 'none'
+  get('.cardapio-drink').style.display = 'none'
+  get('.cardapio-pateis').style.display = 'none'
+}
+
+// pizza enviroment
+let names = pizzaaopen.map(e => e.name)
+let description = pizzaaopen.map(e => e.description)
+let getname = names
+let txt = ''
+let destxt = ''
+getname.forEach(namegt)
+description.forEach(item => {
+  destxt += ' ' + `<div>${item}</div>`
+})
+console.log(destxt)
+function namegt(item) {
+  txt +=
+    ' ' + `<div class="pizza-name">${item}</div>` + `<p>${(destxt += ' ')}</p>`
+}
+get('.pizza-item').innerHTML = txt
+
+//************
+// Cachorro Quente enviroment
+let namesQ = CachorroQ.map(e => e.name)
+let txtQuente = ''
+namesQ.forEach(checknamesQ)
+
+function checknamesQ(item) {
+  txtQuente += ' ' + `<div class="quente-name">${item}</div>` + '<br>'
+}
+get('.quente-item').innerHTML = txtQuente
+/************* */
+// sanduiches
+let sanduicheN = sanduiches2.map(e => e.name)
+let txtsaduinches = ''
+sanduicheN.forEach(item => {
+  txtsaduinches += ' ' + `<div class="sanduíches-name">${item}</div>` + '<br>'
+})
+get('.sanduíches-item').innerHTML = txtsaduinches
+/**************** */
+//´pATEIS
+let pateisN = pateis.map(e => e.name)
+let txtpateis = ''
+pateisN.forEach(item => {
+  txtpateis += ' ' + `<div class="pateis-name">${item}</div>`
+})
+
+get('.pasteis-item').innerHTML = txtpateis
+/*bebidas*/
+let bebidaN = bebidas.map(e => e.name)
+let txtbebida = ''
+bebidaN.forEach(item => {
+  txtbebida += ' ' + `<div class="bebida-name">${item}</div>`
+})
+get('.bebida-item').innerHTML = txtbebida
+/**acaiecremes */
+let acaiemN = acaiecreme.map(e => e.name)
+let txtacaicreme = ''
+acaiemN.forEach(item => {
+  txtacaicreme += ' ' + `<div class="creme-name">${item}</div>`
+})
+get('.creme-item').innerHTML = txtacaicreme
